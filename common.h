74aa0f6f1c72702f7e1295c2284248f551555bd1
Tibor Varga <tiborprogrammer@yahoo.com>
2020-05-13 18:50:56 +0200

    Test branch merge.

---
+++ common.h
@@ -0,0 +1,18 @@
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+
+//Change
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+#define DEBUG_TRACE_EXECUTION
+
+#endif
